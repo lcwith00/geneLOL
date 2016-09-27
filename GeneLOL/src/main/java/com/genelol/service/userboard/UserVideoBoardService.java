7c4973f9c06adcd80f9f18d4551281efbc8dc69b
zionchoi <needgumo@gmail.com>
2016-09-27 14:36:43 +0900

    controlloer incomplete

---
+++ GeneLOL/src/main/java/com/genelol/service/userboard/UserVideoBoardService.java
@@ -0,0 +1,24 @@
+package com.genelol.service.userboard;
+
+import java.util.List;
+
+import com.genelol.vo.userboard.UserVideoBoardVO;
+
+public interface UserVideoBoardService {
+
+	public List<UserVideoBoardVO> userVideoBoardList() throws Exception; // 동영상
+	// 목록보기
+
+	public void videoRegist(UserVideoBoardVO uvbvo) throws Exception; // 동영상올리기
+
+	public UserVideoBoardVO read(Integer board_no) throws Exception; //동영상 읽기
+
+	public void videoModify(UserVideoBoardVO uvbvo) throws Exception;// 수정하기
+
+	public void videoRemove(Integer board_no) throws Exception; // 동영상 삭제
+
+	public UserVideoBoardVO videoBoardDetail(UserVideoBoardVO uvbvo) throws Exception; // 동영상
+	// 상세보기
+
+	
+}
---
+++ GeneLOL/src/test/java/com/genelol/dao/test/UserVideoBoardDAOtest.java
@@ -0,0 +1,63 @@
+package com.genelol.dao.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.genelol.dao.userboard.UserVideoBoardDao;
+import com.genelol.vo.userboard.UserVideoBoardVO;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import oracle.net.aso.d;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
+
+public class UserVideoBoardDAOtest {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserVideoBoardDAOtest.class);
+
+	@Inject
+	private UserVideoBoardDao dao;
+
+	// @Test
+	// public void testVideoInsert() throws Exception {
+	//
+	// UserVideoBoardVO videoBoard = new UserVideoBoardVO();
+	// videoBoard.setBoard_id("2");
+	// videoBoard.setUserid(1);
+	// videoBoard.setBoard_title("되돌아온허디 매드무비");
+	// videoBoard.setBoard_content("이즈장인이 펼치는 갈라쇼");
+	// dao.videoInsert(videoBoard);
+	// }
+
+	// 비디오 읽기 테스트
+	@Test
+	public void testRead() throws Exception {
+
+		logger.info(dao.videoRead(1).toString());
+
+	}
+
+	// 비디오 수정 테스트
+	@Test
+	public void testUpdate() throws Exception {
+
+		UserVideoBoardVO videoBoard = new UserVideoBoardVO();
+		videoBoard.setBoard_title("수정된 글입니다.");
+		videoBoard.setBoard_content("수정 테스트");
+		videoBoard.setBoard_file("file2");
+		videoBoard.setBoard_no(1);
+		dao.videoUpdate(videoBoard);
+	}
+
+	// 비디오 삭제 테스트
+	@Test
+	public void testDelete() throws Exception {
+		dao.videoDelete(1);
+	}
+}
