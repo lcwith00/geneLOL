6c5a0edf9e78c4831ede443ca9c39328a89658cc
Cheolwon <lcw@lcw.local>
2016-10-19 10:33:01 +0900

    get recent game  form incomplete

--- GeneLOL/src/main/java/com/genelol/dao/riotgames/RiotGamesDaoImp.java
+++ GeneLOL/src/main/java/com/genelol/dao/riotgames/RiotGamesDaoImp.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import net.rithms.riot.dto.Static.Champion;
 import net.rithms.riot.dto.Summoner.Summoner;
 
 @Repository
@@ -17,12 +18,32 @@ public class RiotGamesDaoImp implements RiotGamesDao {
 
 	@Override
 	public Summoner getSummoner(long summonerID) {
-		return session.selectOne(namespace + ".summonerSelect",summonerID);
+		return session.selectOne(namespace + ".summonerSelect", summonerID);
 	}
 
 	@Override
 	public int insertSummoner(Summoner summoner) {
-		return session.insert(namespace+".summonerInsert", summoner);
+		return session.insert(namespace + ".summonerInsert", summoner);
+	}
+
+	@Override
+	public Champion getChampion(long championID) {
+		return session.selectOne(namespace + ".championSelect", championID);
+	}
+
+	@Override
+	public int insertChampion(Champion champion) {
+		return session.insert(namespace + ".championInsert", champion);
+	}
+
+	@Override
+	public int updateSummoner(Summoner summoner) {
+		return session.insert(namespace + ".summonerUpdate", summoner);
+	}
+
+	@Override
+	public Summoner getSummonerDetail(Summoner summoner) {
+		return session.selectOne(namespace + ".summonerDetailSelect", summoner);
 	}
 
 }
--- GeneLOL/src/main/java/com/genelol/service/riotgames/RiotGamesService.java
+++ GeneLOL/src/main/java/com/genelol/service/riotgames/RiotGamesService.java
@@ -4,26 +4,32 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import net.rithms.riot.dto.Champion.Champion;
 import net.rithms.riot.dto.Game.Game;
 import net.rithms.riot.dto.League.League;
+import net.rithms.riot.dto.Static.Champion;
+import net.rithms.riot.dto.Static.SummonerSpellList;
 import net.rithms.riot.dto.Stats.RankedStats;
 import net.rithms.riot.dto.Summoner.Summoner;
 
 public interface RiotGamesService {
 	public ArrayList<Game> getRecentRocordBySummonerID(long summonerID);
 
-	public HashMap<String, Summoner> getSummonerBySummonerID(long summonerID);
+	public HashMap<Long, Summoner> getSummonerBySummonerID(long summonerID);
 
 	public Summoner getSummonerBySummonerName(String summonerName);
 
 	public String getEntity(URI uri, int apiIndex);
 
-	public HashMap<String, Summoner> getAllSummoner(ArrayList<Game> games);
+	public HashMap<Long, Summoner> getAllSummoner(ArrayList<Game> games);
 
 	public League getLeagueEntryBySummonerID(long summonerID);
 
 	public RankedStats getRankedStatsBySummonerID(long summonerID, String season);
 
-	public HashMap<String, Champion> getAllChampion(ArrayList<Game> games, String champData);
+	public HashMap<Integer, Champion> getRecentPlayedChampion(ArrayList<Game> games, String champData);
+
+	public HashMap<Integer, Champion> getRankedPlayedChampion(RankedStats rankedStats, String champData);
+
+	public SummonerSpellList getAllSpell(String spellData);
+
 }
