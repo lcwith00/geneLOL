31754de99763a81e2c78b403a0df854229bb3665
DESKTOP-2OE0B0G\kys <kimys2248@gmail.com>
2016-10-28 10:50:41 +0900

    table insert

--- GeneLOL/src/main/resources/db/db_boardtable.sql
+++ GeneLOL/src/main/resources/db/db_boardtable.sql
@@ -1,39 +1,96 @@
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'1','해물파전 편집본','qWVLNsZyJQs',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);
+
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'2','이번엔 진으로돌아왔다! 돌아온허디','91G3FRfYsb8',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'1','good','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','good','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','good','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'1','video test1','qWVLNsZyJQs',sysdate,1,4);
+values('videoLOL',board_no_seq.nextval,'1','good','qWVLNsZyJQs',sysdate,1,4);
+
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'3','뽀글이면 lux 매드무비','VgPazjrf1Ug',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'1','해물파전 편집본','qWVLNsZyJQs',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'4','허디! 시야석가요! 미포서폿! 서정민입니다','31bFjCBAaZg',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'2','이번엔 진으로돌아왔다! 돌아온허디','91G3FRfYsb8',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'5','말파 그자체 엄사스케','lzkNHfxfDCw',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'3','뽀글이면 lux 매드무비','VgPazjrf1Ug',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'6','가끔싸도 괜찮아 FearMe 녹턴','_oQ_h9q0V_c',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'4','허디! 시야석가요! 미포서폿! 서정민입니다','31bFjCBAaZg',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'1',' 본격 페이커 방송','-dRmUXICoZQ',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'5','말파 그자체 엄사스케','lzkNHfxfDCw',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'2','랄구 케이틀린!','y46sNFhbkF8',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'6','가끔싸도 괜찮아 FearMe 녹턴','_oQ_h9q0V_c',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'3','초토화모드!100레벨 clear','0H0qC5VfAaQ',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'1',' 본격 페이커 방송','-dRmUXICoZQ',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'4','미드 아칼리 로이조!','Py93P5KxCak',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'2','랄구 케이틀린!','y46sNFhbkF8',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'5','애니 시도는안해 매드무비','Sb4MvHQtm1g',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'3','초토화모드!100레벨 clear','0H0qC5VfAaQ',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'6','카이팅의 모든것! 거상2 베인','RwgxnOKkChM',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'4','미드 아칼리 로이조!','Py93P5KxCak',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'1','해물파전 편집본','qWVLNsZyJQs',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'5','애니 시도는안해 매드무비','Sb4MvHQtm1g',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'2','다이애나 - 웃긴가?','IWbEA-S1vV8',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'6','카이팅의 모든것! 거상2 베인','RwgxnOKkChM',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'3','뽀글이면 lux 매드무비','VgPazjrf1Ug',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'1','해물파전 편집본','qWVLNsZyJQs',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'4','본격 승급전 양아리엘','9nk_w4fRnxQ',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'2','다이애나 - 웃긴가?','IWbEA-S1vV8',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'5','갱킹의 지존 Zeus','d4FRhaMVc-E',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'3','뽀글이면 lux 매드무비','VgPazjrf1Ug',sysdate,1,0);
 insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
-values('videoLOL',board_no_seq.nextval,'6','허디급 칼리! 전광석화','9Cl-nWJ7jYk',sys date,1,0);
+values('videoLOL',board_no_seq.nextval,'4','본격 승급전 양아리엘','9nk_w4fRnxQ',sysdate,1,0);
+insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'5','갱킹의 지존 Zeus','d4FRhaMVc-E',sysdate,1,0);
+insert into boardtable(board_id,board_no,userid,board_title,board_content,board_date,board_recomm,board_count)
+values('videoLOL',board_no_seq.nextval,'6','허디급 칼리! 전광석화','9Cl-nWJ7jYk',sysdate,1,0);
 
 
 
